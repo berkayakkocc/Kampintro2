06f667cd966a877900bec214c65d6ffedaef6960
berkay akkoc <berkayakkocc6@gmail.com>
2021-01-24 00:19:06 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtıyacKrediManager = new IhtıyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            
+            ILoggerService DatabaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLogerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new SmsLoggerService(), new FileLoggerService() };
+            
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),loggers);
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtıyacKrediManager,tasitKrediManager  };
+
+
+        }
+    }
+}
